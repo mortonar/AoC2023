8f2ad43870b6d7dc2ecf27fa063e5aee666de60b
Alex Morton <mortonar01@gmail.com>
2023-12-11 17:40:22 -0500

    Day 9

---
+++ 9-1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+seq_ends = $stdin.readlines.map(&:strip).map do |line|
+  sequence = line.split.map(&:to_i)
+  sequences = []
+  loop do
+    break if sequence.all? { |num| num == 0 }
+    sequences.push(sequence)
+    sequence = sequence.each_cons(2).map { |n1, n2| n2 - n1 }
+  end
+
+  current = 0
+  until sequences.empty?
+    sequence = sequences.pop
+    current = sequence.last + current
+  end
+  current
+end
+puts seq_ends.reduce(:+)
